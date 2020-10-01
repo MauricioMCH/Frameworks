cd59e94ce3a0501aa78ff69c61870f706651c40b
MauricioMCH <71183060+MauricioMCH@users.noreply.github.com>
2020-09-30 23:10:12 -0500

    Create index.php
    Pagina de prueba

---
+++ index.php
@@ -0,0 +1,19 @@
+
+      <div class="starter-template">
+    <br>
+    <br>
+      <div class="jumbotron" style="background-color:seagreen !important;">
+      <div class="container">
+
+      
+
+      
+      <h1 style="color: white"><strong>Mauricio Morales Chanona</strong> </h1>
+      
+      <p style="color: gray"> Frameworks </p>
+   
+       
+    </div>
+  </div>
+  </div>
+  </div>
